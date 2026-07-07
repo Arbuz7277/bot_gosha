9bf6ca8d8aa8fa938f32baf1792812eb9ac665bc
arbuz <gosha.telegrambot.com>
2026-07-07 21:34:35 +0000

    Edit main.py and update .gitignore

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 # main.p
 
-IP = "129.150.55.165" 
-PORT = "1080"
-TYPE_PROXY = "socks5"
-
-import os
-import time
-import random
-import json
-import socket
 import logging
 import telebot
-from telebot import apihelper
-import requests
-import threading
-from datetime import datetime, timedelta
 from dotenv import load_dotenv
+
 logging.basicConfig(level=logging.INFO,
 format="%(asctime)s : %(name)s %(levelname)s [%(filename)s:%(lineno)s] - %(message)s")
+
 from handlers.commands import setup_handlers
 from utils import *
 
+# Для улучшения читаемости логов
 class Color:
     RED = '\033[91m'
     GREEN = '\033[92m'
@@ -29,15 +19,7 @@ class Color:
 
 logger = logging.getLogger(__name__)
 
-
 logger.info("Program is starting")
-
-"""
-apihelper.proxy = {"https": f"{TYPE_PROXY}://{IP}:{PORT}"}
-apihelper.CONNECT_TIMEOUT = 15
-apihelper.READ_TIMEOUT = 15
-apihelper.SESSION_TIME_TO_LIVE = 5 * 60
-"""
 
 load_dotenv('secrets.env')
 API_TOKEN = os.getenv('TOKEN')
@@ -52,46 +34,16 @@ def main():
     """Запуск бота"""
     bot = create_bot()
     
-    running_bot = True
-
-    def exit():
-        nonlocal running_bot
-        logger.info(f'{Color.YELLOW}EXIT{Color.RESET}')
-        running_bot = False
-
     try:
-        err = False
-        while True:
-            if running_bot:
-                try:
-                    logger.info(f'{Color.GREEN}Bot running...{Color.RESET}')
-                    if err:
-                        for uid in OWNER:
-                            bot.send_message(uid, f'Error: {err}')
-                    bot.infinity_polling(timeout=15, long_polling_timeout=5, allowed_updates=['message', 'callback_query', 'inline_query', 'my_chat_member'])
-                    time.sleep(5)
-                except socket.gaierror as e:
-                    logger.error(f'{Color.RED}Connection error. Error code: {e}{Color.RESET}')
-                except ConnectionError as e:
-                    logger.error(f'{Color.RED}Connection error. There in no Wi-Fi.{Color.RESET}')
-                except KeyboardInterrupt:
-                    exit()
-                except Exception as e:
-                    logger.error(f'{Color.RED}Error: {e}{Color.RESET}')
-                    err = e
-                    for uid in OWNER:
-                        bot.send_message(uid, f'Error: {e}')
-                    time.sleep(5)
-                    print("START")
-                except:
-                    logger.error(f'{Color.RED}UNKNOW ERROR{Color.RESET}')
-            else:
-                logger.info(f"{Color.YELLOW}Program stop due to admin.{Color.RESET}")
-                break
-    
+        logger.info(f'{Color.GREEN}Bot running...{Color.RESET}')
+
+        # Поллинг бота
+        bot.infinity_polling(timeout=15, long_polling_timeout=5, allowed_updates=['message', 'callback_query', 'inline_query', 'my_chat_member'])
     except KeyboardInterrupt:
-        logger.info(f'{Color.YELLOW}EXIT{Color.RESET}')
-        running_bot = False
+        logger.info("Program finished.")
+    except Exception as e:
+        logger.error(f'{Color.RED}Error: {e}{Color.RESET}. Restarting in 5s.')
+        time.sleep(5)
 
 if __name__ == "__main__":
     main()
